aeaeaf2eca4f986edcc08585605965f7d8d7ee5e
BigBen <faninyou@gmail.com>
2026-01-24 14:59:35 -0500

    Create Gui.cpp

---
+++ Source/Core/Scripting/Gui.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+#include "imgui.h"
+#include "API/Gui.h"  // adjust if your header path differs
+
+namespace API::Gui
+{
+    void DrawText(ImVec2 pos, unsigned int color, std::string text)
+    {
+        ImGui::GetForegroundDrawList()->AddText(
+            pos,
+            color,
+            text.c_str()
+        );
+    }
+}
